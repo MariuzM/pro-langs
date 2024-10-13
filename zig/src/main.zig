e877200929264f2d37e9c38f9a1a56d9ecf621b7
MariuzM <info@marius.dev>
2024-10-13 14:19:25 +0700

    add more examples

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,6 +1,32 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-pub fn main() void {
-    const name = "Hello, World!";
-    std.debug.print("{s}", .{name});
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var n: usize = 0;
+    if (builtin.target.os.tag == .macos) {
+        n = 10;
+    } else {
+        n = 12;
+    }
+    const buffer = try allocator.alloc(u64, n);
+    std.debug.print("{d}\n", .{buffer});
+    const slice = buffer[0..];
+    for (slice) |x| {
+        std.debug.print("{d}\n", .{x});
+    }
+
+    // ==================================================================
+
+    // const t2 = [10]u64{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    // const slice = t2[1..4];
+    // std.debug.print("{d}\n", .{slice.len});
+
+    // ==================================================================
+
+    // const t1 = "Hello, World!";
+    // std.debug.print("{s}\n", .{t1});
+
+    // ==================================================================
 }
