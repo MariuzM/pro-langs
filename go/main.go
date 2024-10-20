7cff2d17ba8e703bbf660ddabe54f6c31e84c21c
MariuzM <info@marius.dev>
2024-10-20 23:45:12 +0700

    add simple go html template render

--- go/main.go
+++ go/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
@@ -12,12 +13,18 @@ func main() {
 
 	fmt.Println("Server on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to our Go server!")
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
 }
 
 func handleAbout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About us")
+	fmt.Fprintf(w, "About")
 }
