f0ac2f7acb1a737130a1e33e805f2858148dd55a
MariuzM <info@marius.dev>
2024-10-21 12:48:02 +0700

    go add pointer examples

--- go/main.go
+++ go/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"text/template"
 )
@@ -10,9 +9,10 @@ import (
 func main() {
 	http.HandleFunc("/", handleHome)
 	http.HandleFunc("/about", handleAbout)
+	pointerExamples()
 
-	fmt.Println("Server on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// fmt.Println("Server on http://localhost:8080")
+	// log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
@@ -27,4 +27,42 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 
 func handleAbout(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About")
+}
+
+func pointerExamples() {
+	// Example 1: Basic pointer usage
+	fmt.Printf("=====================================================\n")
+	x := 10
+	ptr := &x
+	fmt.Printf("Value of x: %d\n", x)
+	fmt.Printf("Address of x: %p\n", ptr)
+	fmt.Printf("Value at address stored in ptr: %d\n", *ptr)
+	fmt.Printf("=====================================================\n")
+
+	// Example 2: Modifying value through pointer
+	*ptr = 20
+	fmt.Printf("New value of x: %d\n", x)
+	fmt.Printf("=====================================================\n")
+
+	// Example 3: Pointer to a struct
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	person := Person{"Alice", 30}
+	personPtr := &person
+	fmt.Printf("Person: %+v\n", person)
+	personPtr.Age = 31
+	fmt.Printf("Updated Person: %+v\n", person)
+
+	// Example 4: Pointer to a slice
+	slice := []int{1, 2, 3}
+	modifySlice(&slice)
+	fmt.Printf("Modified slice: %v\n", slice)
+	fmt.Printf("=====================================================\n")
+}
+
+func modifySlice(s *[]int) {
+	*s = append(*s, 4)
 }
